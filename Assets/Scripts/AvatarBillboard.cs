df9c78abe4aafb4549faf91870984d5bf6ae7ede
brnolson <brnolson33@gmail.com>
2025-12-03 17:02:53 -0600

    billboard fixed
    Should be functioning properly now

--- Assets/Scripts/AvatarBillboard.cs
+++ Assets/Scripts/AvatarBillboard.cs
@@ -44,14 +44,4 @@ public class AvatarBillboard : MonoBehaviour
     {
         textMesh.text = text;
     }
-
-    void LateUpdate()
-    {
-        if (Camera.main != null)
-        {
-            var cam = Camera.main.transform;
-            transform.LookAt(cam.position);
-            transform.Rotate(0, 180f, 0);
-        }
-    }
 }
